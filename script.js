8151fd9332becdc6a8f5e7028333803ab721cee6
Muhammad-Ayub830 <muhammadayubmrk@gmail.com>
2025-04-30 15:47:22 +0500

    half of the first page is developed

---
+++ script.js
@@ -0,0 +1,25 @@
+var menu_btn = document.getElementById("bars");
+var list = document.getElementById("list");
+var search = document.getElementById("search");
+menu_btn.addEventListener("click",function(){
+    if(list.style.height == "0px"){
+        list.style.height= "200px"
+        list.style.padding = "20px"
+         list.style.borderBottom = "1px solid black"
+    }else{
+         list.style.height = "0px"
+          list.style.padding = "0px"
+           list.style.borderBottom = ""
+    }
+})
+search.addEventListener("click",function(){
+    if(list.style.height == "0px"){
+        list.style.height= "180px"
+        list.style.padding = "20px"
+        list.style.borderBottom = "1px solid black"
+    }else{
+         list.style.height = "0px"
+          list.style.padding = "0px"
+          list.style.borderBottom = ""
+    }
+})
